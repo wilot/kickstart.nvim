2b6f95f89e60fcc0be60cd52aeda35530dea6880
William Thornley <williamothornley@gmail.com>
2024-02-29 16:13:25 +0000

    Add relative line numbers

--- init.lua
+++ init.lua
@@ -299,6 +299,9 @@ vim.o.hlsearch = false
 -- Make line numbers default
 vim.wo.number = true
 
+-- Make line numbers relative
+vim.wo.relativenumber = true
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
